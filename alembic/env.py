ec231cbb62ed3dc9686b5abe8b39997734b456f5
Ayanleaideed <Ayanleaideed@outlook.com>
2026-03-05 21:40:53 -0600

    chore(lint): sort imports for alembic env and health test

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
-from app.db.base import Base
 from app.core.config import get_settings
+from app.db.base import Base
 from app.db.models import *  # noqa: F403
 from sqlalchemy import engine_from_config, pool
 
@@ -47,5 +47,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
-
